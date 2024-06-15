c72f47cde41e8d404b1218a0ea7cb0eaeb90c7c6
DainfromLiria <mayor3217@gmail.com>
2024-06-15 15:11:05 +0200

    [Rework] Update Main settings.

---
+++ src/utils/settings.py
@@ -0,0 +1,18 @@
+"""
+    Main application settings.
+"""
+BASE = {
+    'WIDTH': 1280,
+    'HEIGHT': 800,
+    'BARRIER_COUNT': 100
+}
+STATES = {
+    "BARRIER": 1,
+    "FREE": 2,
+    "FOUND": 3
+}
+LIMITS = {
+    'x_max': 20,
+    'y_max': 15,
+    'z_max': 30
+}
