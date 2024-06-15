dfa64552799f5392c8319944bf489cb5d4fb39c4
DainfromLiria <mayor3217@gmail.com>
2024-06-15 15:07:32 +0200

    [Rework] Add documentation for Pos class.

---
+++ src/location/pos.py
@@ -0,0 +1,67 @@
+"""
+    Class represented one cube (position) in 3D location
+"""
+from math import inf
+from typing import Tuple
+
+
+class Pos:
+    """
+        Class represented one cube (position) in 3D location.
+
+        Attributes:
+            prev - Tuple[int, int, int] - previous position.
+            We were in this position in previous step of the AStar algorithm.
+
+            state - int - state of this position. Can be:
+                1 - BARRIER - wall, floor or roof
+                2 - FREE - empty space
+                3 - FOUND - visited position
+
+            dist - int - distance from start.
+            On the beginning of the AStar algorithm, all positions is
+            on the max distance from start point, so we can use inf.
+    """
+    def __init__(self, state: int):
+        """Initialize one position."""
+        self.prev = (-1, -1, -1)
+        self.state = state
+        self.dist = inf
+
+    def check_state(self, state: int) -> bool:
+        """Check if input state is same with state of current position.
+        Attributes:
+            state - int - state of this position. Valid values is documented in the class docstring.
+        Return:
+            bool - True if states are same, False otherwise.
+        """
+        return self.state == state
+
+    def get_prev(self) -> Tuple[int, int, int]:
+        """Return previous position set during AStar algorithm."""
+        return self.prev
+
+    def set_prev(self, prev: Tuple[int, int, int]) -> None:
+        """Set new previous position.
+        Attributes:
+            prev - Tuple[int, int, int] - previous position in 3D location.
+        """
+        self.prev = prev
+
+    def set_dist(self, dist: float) -> None:
+        """Set new distance to start position.
+        Attributes:
+            dist - int - new distance to start.
+        """
+        self.dist = dist
+
+    def get_dist(self) -> float:
+        """Return distance to start position."""
+        return self.dist
+
+    def set_state(self, state: int) -> None:
+        """Set new state of this position.
+        Attributes:
+            state - int - new state.
+        """
+        self.state = state
